703819017fccaa56ef031e15067a93cf882ec390
Andre Almeida <aralmeida99@gmail.com>
2021-11-10 22:23:19 +0000

    Moved ros_ci_dep.sh to outside the .github/workflows

--- ros_ci_dependencies.sh
+++ ros_ci_dependencies.sh
@@ -4,6 +4,7 @@
 sudo apt-get install ros-noetic-ros-controllers ros-noetic-ackermann-msgs
 
 # Camera dependencies
+" Uncomment when ready
 sudo apt-get install git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev ros-noetic-rgbd-launch
 git clone git://github.com/OpenKinect/libfreenect.git
 cd libfreenect
@@ -19,5 +20,6 @@ git clone https://github.com/ros-drivers/freenect_stack.git
 cd ..
 catkin_make
 source devel/setup.sh
+"
 ##roslaunch freenect_launch freenect.launch depth_registration:=true
 
